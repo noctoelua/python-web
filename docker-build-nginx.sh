afffce0b2be9e242873a30c813bd3b177d698fc6
reo-sakairi <reo-sakairi@sig-c.co.jp>
2024-02-24 06:28:30 +0900

    add fluentd

---
+++ docker-build-nginx.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# docker 関連
+rm -rf /var/service/docker
+cp -r /var/vagrantshare/docker /var/service/docker
+
+cd /var/service/docker
+
+docker container prune -f
+docker image prune -f
+
+docker compose -f docker-compose-dev.yml rm -fsv shizai-nginx
+docker-compose -f docker-compose-dev.yml up -d --no-deps --build shizai-nginx
